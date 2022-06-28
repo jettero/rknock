cfce16599817aa198688f1a2a83a13b5791b05f5
Paul Miller <paul@jettero.pl>
2022-06-28 11:02:00 -0400

    blah

--- Makefile
+++ Makefile
@@ -1,9 +1,22 @@
 
+default: last
 
-default: run
+last:
+	@+ make --no-print-directory "$$(cat .last 2>/dev/null || echo run)"
+
+door-help:
+	@ echo $@ > .last
+	cargo run --bin door -- --help
 
 run:
+	@ echo $@ > .last
 	cargo run --bin door & (sleep 0.5; cargo run --bin knock)
 
-clean build:
+build:
+	@ echo $@ > .last
 	cargo $@
+
+clean:
+	cargo clean
+	git clean -dfx
+
--- src/door.rs
+++ src/door.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use data_encoding::BASE64;
 use ring::hmac;
 use std::io::ErrorKind;
@@ -14,34 +15,47 @@ fn split_payload(buf: &[u8]) -> Result<(&[u8], &[u8]), std::io::ErrorKind> {
 }
 
 fn main() -> Result<(), ring::error::Unspecified> {
-    let pass = "supz";
-    let key = hmac::Key::new(hmac::HMAC_SHA256, pass.as_bytes());
-    let socket = UdpSocket::bind("localhost:22022").expect("couldn't bind to socket");
+    let matches = App::new("door is door")
+        .version("0.0.0")
+        .author("Paul Miller <paul@jettero.pl>")
+        .about("Watches the doors and listens for the secret codes")
+        .arg(Arg::with_name("verbose").long("verbose").short('v')
+        .arg(Arg::with_name("listen").long("listen").short('l').takes_value(true)
+             .help("where to listen for the codes. default: localhost:22"))
+        .arg(Arg::with_name("secret").long("secret").short('s').takes_value(true)
+             .help("do not use this in production, use a config file or something; handy for testing. default: secret"))
+        .get_matches();
+
+    let verbose = matches.value_of("verbose");
+    let key = hmac::Key::new(hmac::HMAC_SHA256, matches.value_of("secret").unwrap_or("secret").as_bytes());
+    let socket = UdpSocket::bind(matches.value_of("listen").unwrap_or("localhost:22"))
+        .expect("couldn't bind to socket");
 
     let mut buf = [0; 256];
-    let (amt, src) = socket
-        .recv_from(&mut buf)
-        .expect("couldn't read from buffer");
+    let (amt, src) = socket.recv_from(&mut buf).expect("couldn't read from buffer");
     let (nonce, tag) = split_payload(&buf[..amt]).unwrap();
-
     let dtag = BASE64.decode(tag).unwrap();
 
-    let verified = match hmac::verify(&key, nonce, &dtag) {
+    let verified = match hmac::verify(&key, &nonce, &dtag) {
         Ok(_) => true,
         Err(_) => false,
     };
 
-    println!(
-        "heard: amt={} src={} nonce={} tag={} {}",
-        amt,
-        src,
-        from_utf8(nonce).unwrap(),
-        from_utf8(tag).unwrap(),
-        match verified {
-            true => "verified",
-            false => "FAILCOPTER",
-        }
-    );
+    if args.verbose {
+        let inonce: u64 = from_utf8(&nonce).unwrap().parse::<u64>().unwrap();
+        let stag: &str = from_utf8(&tag).unwrap();
+        println!(
+            "heard: amt={} src={} nonce={} dtag={} {}",
+            amt,
+            src,
+            inonce,
+            stag,
+            match verified {
+                true => "verified",
+                false => "FAILCOPTER",
+            }
+        );
+    }
 
     Ok(())
 }
