81b3bf12b97b6248a8d47ddcd314b618ff897061
Paul Miller <paul@jettero.pl>
2022-07-04 11:48:11 -0400

    split listening into a new function

--- src/door.rs
+++ src/door.rs
@@ -18,40 +18,8 @@ fn split_payload(buf: &[u8]) -> Result<(&[u8], &[u8]), std::io::ErrorKind> {
     Err(ErrorKind::NotFound)
 }
 
-fn get_args() -> (bool, String, String) {
-    let matches = App::new("door is door")
-        .version("0.0.0")
-        .author("Paul Miller <paul@jettero.pl>")
-        .about("Watches the doors and listens for the secret codes")
-        .arg(arg!(verbose: -v --verbose).action(ArgAction::SetTrue))
-        .arg(arg!(listen: -l --listen).value_parser(value_parser!(String)).default_value("localhost:20022"))
-        .arg(arg!(secret: -s --secret).value_parser(value_parser!(String)).default_value("secret"))
-        .get_matches();
-
-    let verbose = matches.get_one::<bool>("verbose").expect("defaulted by clap");
-    let key = matches.get_one::<String>("secret").expect("defaulted by clap").to_string();
-    let listen = matches.get_one::<String>("listen").expect("defaulted by clap").to_string();
-
-    return (*verbose, key, listen);
-}
-
-fn main() -> Result<(), ring::error::Unspecified> {
-    let formatter = Formatter3164 {
-        facility: Facility::LOG_DAEMON,
-        process: "knock-door".into(),
-        hostname: None,
-        pid: 0,
-    };
-
-    let logger = syslog::unix(formatter).unwrap();
-
-    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
-        .map(|()| log::set_max_level(LevelFilter::Info))
-        .expect("logging setup failure");
-
-    let (verbose, key_str, listen) = get_args();
+fn listen_to_msgs(verbose: bool, listen: String, key: &hmac::Key) {
     let socket = UdpSocket::bind(listen.as_str()).expect("couldn't bind to socket");
-    let key = hmac::Key::new(hmac::HMAC_SHA256, key_str.as_bytes());
 
     if verbose {
         // we use listen.as_str() above so we don't "move" listen to the bind()
@@ -90,6 +58,43 @@ fn main() -> Result<(), ring::error::Unspecified> {
             }
         );
     }
+}
+
+fn get_args() -> (bool, String, String) {
+    let matches = App::new("door is door")
+        .version("0.0.0")
+        .author("Paul Miller <paul@jettero.pl>")
+        .about("Watches the doors and listens for the secret codes")
+        .arg(arg!(verbose: -v --verbose).action(ArgAction::SetTrue))
+        .arg(arg!(listen: -l --listen).value_parser(value_parser!(String)).default_value("localhost:20022"))
+        .arg(arg!(secret: -s --secret).value_parser(value_parser!(String)).default_value("secret"))
+        .get_matches();
+
+    let verbose = matches.get_one::<bool>("verbose").expect("defaulted by clap");
+    let key = matches.get_one::<String>("secret").expect("defaulted by clap").to_string();
+    let listen = matches.get_one::<String>("listen").expect("defaulted by clap").to_string();
+
+    return (*verbose, key, listen);
+}
+
+fn main() -> Result<(), ring::error::Unspecified> {
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_DAEMON,
+        process: "knock-door".into(),
+        hostname: None,
+        pid: 0,
+    };
+
+    let logger = syslog::unix(formatter).unwrap();
+
+    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+        .map(|()| log::set_max_level(LevelFilter::Info))
+        .expect("logging setup failure");
+
+    let (verbose, key_str, listen) = get_args();
+    let key = hmac::Key::new(hmac::HMAC_SHA256, key_str.as_bytes());
+
+    listen_to_msgs(verbose, listen, &key);
 
     Ok(())
 }
