98225e61f5ba7db9b3f56f91943581c4eabdbabd
Paul Miller <paul@jettero.pl>
2022-08-23 12:09:56 -0400

    oop hmac frobnicator

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,31 @@ pub fn get_key(mut key_str: String) -> hmac::Key {
     return hmac::Key::new(hmac::HMAC_SHA256, key_str.as_bytes());
 }
 
-pub fn hmac_sign(nonce: &str, key: &str) -> String {
-    let internal = format!("{}:{}", nonce, key);
-    let mut hasher = Sha256::new();
-    hasher.update(internal.as_bytes());
-    let res = hasher.finalize(); // GenericArray<u8, usize>
-    return format!("{}:{}", nonce, BASE64.encode(&res[..]));
+
+pub struct HMACFrobnicator {
+    key: String
+}
+
+impl HMACFrobnicator {
+    pub fn new(key: &str) -> Self {
+        let mut key_str: String = key.to_string();
+        if key_str.starts_with("@") {
+            let fname = &key_str[1..];
+            key_str = fs::read_to_string(fname).expect("couldn't read file");
+            key_str = key_str.trim().to_string();
+        }
+        HMACFrobnicator {
+            key: key_str,
+        }
+    }
+
+    pub fn sign(&mut self, msg: &str) -> String {
+        let internal = format!("{}:{}", msg, self.key);
+        let mut hasher = Sha256::new();
+        hasher.update(internal.as_bytes());
+        let res = hasher.finalize(); // GenericArray<u8, usize>
+        return format!("{}:{}", msg, BASE64.encode(&res[..]));
+    }
 }
 
 #[cfg(test)]
@@ -27,14 +46,13 @@ mod tests {
     use super::*;
     #[test]
     fn sign_something() {
-        let msg: String = hmac_sign("1234", "secret key");
+        let mut hmt = HMACFrobnicator::new("secret key");
+        let msg = hmt.sign("1234");
 
         assert_eq!(msg, "1234:iKC5sOqv+cjt3IG3qfQ/B4Xwyvz7069Zl7hGN+7ea2E=");
-        /*
-            echo -n 1234:; echo -n "1234:secret key" | sha256sum | cut -d' ' -f1 \
-                | xxd -r -p | uuencode -m supz | head -n 2 | tail -n 1
-
-            1234:iKC5sOqv+cjt3IG3qfQ/B4Xwyvz7069Zl7hGN+7ea2E=
+        /* echo -n 1234:; echo -n "1234:secret key" | sha256sum | cut -d' ' -f1 \
+             | xxd -r -p | uuencode -m supz | head -n 2 | tail -n 1
+           1234:iKC5sOqv+cjt3IG3qfQ/B4Xwyvz7069Zl7hGN+7ea2E=
         */
     }
 }
